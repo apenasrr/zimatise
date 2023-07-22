de3ed47829b0be43ab06bb8f3a02bd3538aa3dd9
apenasrr <apenasrr@gmail.com>
2023-07-21 22:38:20 -0300

    v0.1.30 Send to multiple channels with cover, desc

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='zimatise',
-    version='0.0.1',
-    description='Workflow to optimize audiovisual collections for streaming, with high customization and bulk uploader.',
-    url='https://github.com/apenasrr/zimatise',
-    author='apenasrr',
-    author_email='apenasrr@gmail.com',
+    name="zimatise",
+    version="0.1.30",
+    description="Workflow to optimize audiovisual collections for streaming, with high customization and bulk uploader.",
+    url="https://github.com/apenasrr/zimatise",
+    author="apenasrr",
+    author_email="apenasrr@gmail.com",
     packages=[],
-    classifiers=['Development Status :: 1 - Planning'],
+    classifiers=["Development Status :: 4 - Beta"],
 )
